856e41488d955b11b6f02f66e16979ea3028f1e0
BrevinCrawford <70293997+BrevinCrawford@users.noreply.github.com>
2020-10-21 16:32:40 -0500

    Add files via upload

---
+++ SuperStack.java
@@ -0,0 +1,30 @@
+
+public class SuperStack {
+	private T[] items;
+	private int top;
+	
+	public SuperStack<T>(int size) {
+		items = new T[];
+		
+	}
+	
+	public boolean isEmpty() {
+		//TODO
+		return true;
+	}
+	
+	public boolean isFull() {
+		//TODO
+		return true;
+	}
+
+	public boolean push(T item) {
+		//TODO
+		return null;
+	}
+	
+	public T pop() {
+		
+	}
+	
+}
